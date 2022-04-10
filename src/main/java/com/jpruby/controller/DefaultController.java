45407ee50ae8d6b415932037705dab24918331bb
jpruby888 <japan8364@163.com>
2022-04-10 16:26:50 +0800

    first commit

---
+++ src/main/java/com/jpruby/controller/DefaultController.java
@@ -0,0 +1,46 @@
+package com.jpruby.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 　　┏┓　　　┏┓+ +
+ * 　┏┛┻━━━┛┻┓ + +
+ * 　┃　　　　　　　┃
+ * 　┃　　　━　　　┃ ++ + + +
+ * ████━████ ┃+
+ * 　┃　　　　　　　┃ +
+ * 　┃　　　┻　　　┃
+ * 　┃　　　　　　　┃ + +
+ * 　┗━┓　　　┏━┛
+ * 　　　┃　　　┃
+ * 　　　┃　　　┃ + + + +
+ * 　　　┃　　　┃    草泥马登场
+ * 　　　┃　　　┃ +  神兽保佑
+ * 　　　┃　　　┃    代码无bug
+ * 　　　┃　　　┃　　+
+ * 　　　┃　 　　┗━━━┓ + +
+ * 　　　┃ 　　　　　　　┣┓
+ * 　　　┃ 　　　　　　　┏┛
+ * 　　　┗┓┓┏━┳┓┏┛ + + + +
+ * 　　　　┃┫┫　┃┫┫
+ * 　　　　┗┻┛　┗┻┛+ + + +
+ * Created with IntelliJ IDEA.
+ * Project: jp-music
+ * Author: jpruby
+ * Date: 2022/04/10/15:43
+ * Description: 一看就会，一写就废
+ * FilePath: com.jpruby.controller
+ * "hello world" is classical
+ * Copyright (c) 2022, All Rights Reserved.
+ */
+@RestController
+@RequestMapping("/test")
+public class DefaultController {
+
+    @GetMapping
+    public String test(){
+        return "hello jpruby";
+    }
+}
---
+++ src/test/java/com/jpruby/JpMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpruby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpMusicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
